d9bc392aaaea81ada561c4422364bd9e12ef3fad
UCC-identitycheck- <Simona.Viscontii@lifefinancialtime.online>
2026-02-26 15:04:27 +0100

    Add files via upload

---
+++ index_files/63665689.js
@@ -0,0 +1,8 @@
+_iub.csRC = { consApiKey: '4lTwILDFdUSyAri10fCZvZua4qhyAnFX', showBranding: false, publicId: 'a4967516-6db6-11ee-8bfc-5ad8d8c564c0', floatingGroup: false };
+_iub.csEnabled = true;
+_iub.csPurposes = [3,1,4,5];
+_iub.cpUpd = 1753172777;
+_iub.csT = 0.05;
+_iub.googleConsentModeV2 = true;
+_iub.totalNumberOfProviders = 4;
+_iub.csSiteConf = {"askConsentAtCookiePolicyUpdate":true,"cookiePolicyInOtherWindow":true,"countryDetection":true,"enableFadp":true,"enableLgpd":true,"enableUspr":true,"lgpdAppliesGlobally":false,"perPurposeConsent":true,"siteId":2541159,"cookiePolicyId":63665689,"lang":"it","banner":{"acceptButtonCaptionColor":"#0E2844","acceptButtonColor":"#F7C505","acceptButtonDisplay":true,"backgroundColor":"#F0F0F0","brandBackgroundColor":"#F0F0F0","brandTextColor":"#000000","closeButtonDisplay":false,"continueWithoutAcceptingButtonCaptionColor":"#0E2844","continueWithoutAcceptingButtonColor":"#F0F0F0","continueWithoutAcceptingButtonDisplay":true,"customizeButtonCaptionColor":"#0E2844","customizeButtonColor":"#BBBBBBBB","customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"logo":"https://www.invenium.it/wp-content/uploads/2023/09/logo-invenium-1200.png","linksColor":"#0E2844","ownerName":"invenium.it","position":"float-bottom-center","rejectButtonCaptionColor":"#0E2844","rejectButtonColor":"#F7C505","rejectButtonDisplay":true,"showPurposesToggles":true,"showTotalNumberOfProviders":true,"textColor":"#0E2844"}};
